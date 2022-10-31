d52dbc267a870af9fcd9b6c13833305197b9ba51
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2022-10-30 21:19:31 -0300

    feat: filter component

--- web/src/Components/Filter/index.tsx
+++ web/src/Components/Filter/index.tsx
@@ -1,1 +1,133 @@
 import React from "react";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Grow from "@mui/material/Grow";
+import Popper from "@mui/material/Popper";
+import MenuItem from "@mui/material/MenuItem";
+import MenuList from "@mui/material/MenuList";
+import Button from "@mui/material/Button";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import Paper from "@mui/material/Paper";
+
+import { useState } from "react";
+
+const options = [
+  "Create a merge commit",
+  "Filtrar demandas por",
+  "Rebase and merge",
+];
+
+const [open, setOpen] = useState(false);
+const anchorRef = React.useRef<HTMLDivElement>(null);
+const [selectedIndex, setSelectedIndex] = useState(1);
+
+const handleClick = () => {
+  console.info(`You clicked ${options[selectedIndex]}`);
+};
+
+const handleMenuItemClick = (
+  event: React.MouseEvent<HTMLLIElement, MouseEvent>,
+  index: number
+) => {
+  setSelectedIndex(index);
+  setOpen(false);
+};
+
+const handleToggle = () => {
+  setOpen((prevOpen) => !prevOpen);
+};
+
+const handleClose = (event: Event) => {
+  if (
+    anchorRef.current &&
+    anchorRef.current.contains(event.target as HTMLElement)
+  ) {
+    return;
+  }
+
+  setOpen(false);
+};
+
+export default function Filter() {
+  return (
+    <div>
+      <React.Fragment>
+        <ButtonGroup
+          variant="contained"
+          ref={anchorRef}
+          aria-label="split button"
+          style={{
+            boxShadow: "#bdbdbd 0px 1px 5px 1px",
+          }}
+        >
+          <Button
+            sx={{
+              backgroundColor: "transparent",
+              color: "#707070",
+            }}
+            style={{
+              backgroundColor: "transparent",
+              borderColor: "#7070706f",
+            }}
+            onClick={handleClick}
+          >
+            {options[selectedIndex]}
+          </Button>
+          <Button
+            sx={{
+              backgroundColor: "transparent",
+              color: "#707070",
+              borderColor: "none",
+            }}
+            style={{ backgroundColor: "transparent" }}
+            size="small"
+            aria-controls={open ? "split-button-menu" : undefined}
+            aria-expanded={open ? "true" : undefined}
+            aria-label="select merge strategy"
+            aria-haspopup="menu"
+            onClick={handleToggle}
+          >
+            <FilterListIcon />
+          </Button>
+        </ButtonGroup>
+        <Popper
+          sx={{
+            zIndex: 1,
+          }}
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom" ? "center top" : "center bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList id="split-button-menu" autoFocusItem>
+                    {options.map((option, index) => (
+                      <MenuItem
+                        key={option}
+                        disabled={index === 2}
+                        selected={index === selectedIndex}
+                        onClick={(event) => handleMenuItemClick(event, index)}
+                      >
+                        {option}
+                      </MenuItem>
+                    ))}
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </React.Fragment>
+    </div>
+  );
+}
--- web/src/Components/SubHeader/index.tsx
+++ web/src/Components/SubHeader/index.tsx
@@ -1,22 +1,12 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
-import ClickAwayListener from "@mui/material/ClickAwayListener";
-import Grow from "@mui/material/Grow";
 import Paper from "@mui/material/Paper";
-import Popper from "@mui/material/Popper";
-import MenuItem from "@mui/material/MenuItem";
-import MenuList from "@mui/material/MenuList";
 import InputBase from "@mui/material/InputBase";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
+import Filter from "../Filter";
 
 import SearchIcon from "@mui/icons-material/Search";
 import ListAltIcon from "@mui/icons-material/ListAlt";
 import GridOnIcon from "@mui/icons-material/GridOn";
-import FilterListIcon from "@mui/icons-material/FilterList";
-
-import { useState } from "react";
 
 import "../../styles/index.css";
 
@@ -25,126 +15,13 @@ export default function subHeader({
   isListFormat,
   setIsListFormat,
 }: any) {
-  const options = [
-    "Create a merge commit",
-    "Filtrar demandas por",
-    "Rebase and merge",
-  ];
-
-  const [open, setOpen] = React.useState(false);
-  const anchorRef = React.useRef<HTMLDivElement>(null);
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
-
-  const handleClick = () => {
-    console.info(`You clicked ${options[selectedIndex]}`);
-  };
-
-  const handleMenuItemClick = (
-    event: React.MouseEvent<HTMLLIElement, MouseEvent>,
-    index: number
-  ) => {
-    setSelectedIndex(index);
-    setOpen(false);
-  };
-
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
-
-  const handleClose = (event: Event) => {
-    if (
-      anchorRef.current &&
-      anchorRef.current.contains(event.target as HTMLElement)
-    ) {
-      return;
-    }
-
-    setOpen(false);
-  };
-
   return (
     <div className="mb-10">
       <div className="flex justify-around items-center shadow-page-title-shadow h-[5rem]">
         <h1 className="text-dark-blue-weg font-bold text-3xl font-roboto">
           {children}
         </h1>
-        <React.Fragment>
-          <ButtonGroup
-            variant="contained"
-            ref={anchorRef}
-            aria-label="split button"
-            style={{
-              boxShadow: "#bdbdbd 0px 1px 5px 1px",
-            }}
-          >
-            <Button
-              sx={{
-                backgroundColor: "transparent",
-                color: "#707070",
-              }}
-              style={{
-                backgroundColor: "transparent",
-                borderColor: "#7070706f",
-              }}
-              onClick={handleClick}
-            >
-              {options[selectedIndex]}
-            </Button>
-            <Button
-              sx={{
-                backgroundColor: "transparent",
-                color: "#707070",
-                borderColor: "none",
-              }}
-              style={{ backgroundColor: "transparent" }}
-              size="small"
-              aria-controls={open ? "split-button-menu" : undefined}
-              aria-expanded={open ? "true" : undefined}
-              aria-label="select merge strategy"
-              aria-haspopup="menu"
-              onClick={handleToggle}
-            >
-              <FilterListIcon />
-            </Button>
-          </ButtonGroup>
-          <Popper
-            sx={{
-              zIndex: 1,
-            }}
-            open={open}
-            anchorEl={anchorRef.current}
-            role={undefined}
-            transition
-            disablePortal
-          >
-            {({ TransitionProps, placement }) => (
-              <Grow
-                {...TransitionProps}
-                style={{
-                  transformOrigin:
-                    placement === "bottom" ? "center top" : "center bottom",
-                }}
-              >
-                <Paper>
-                  <ClickAwayListener onClickAway={handleClose}>
-                    <MenuList id="split-button-menu" autoFocusItem>
-                      {options.map((option, index) => (
-                        <MenuItem
-                          key={option}
-                          disabled={index === 2}
-                          selected={index === selectedIndex}
-                          onClick={(event) => handleMenuItemClick(event, index)}
-                        >
-                          {option}
-                        </MenuItem>
-                      ))}
-                    </MenuList>
-                  </ClickAwayListener>
-                </Paper>
-              </Grow>
-            )}
-          </Popper>
-        </React.Fragment>
+        <Filter />
         <Paper
           component="form"
           sx={{
