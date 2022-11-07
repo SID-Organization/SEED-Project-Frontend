f813e6cb222e7236b7c9ac2715bc0a0f6c2227fd
HenriqueCole <henriquecolefernandes@gmail.com>
2022-11-07 14:08:31 -0300

    refactor: login page

--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -16,94 +16,104 @@ export default function Login() {
   const label = { inputProps: { "aria-label": "Checkbox demo" } };
   return (
     <div className="bg-loginWallpaper bg-cover w-full h-screen">
-      <div className="w-2/6 h-60 gap-24 grid justify-center items-center">
-        <img
-          className="w-32 h-20 drop-shadow-weg-shadow mt-20"
-          src={WegLogo}
-          alt=""
-        />
-        <div className="grid font-bold">
-          <h1 className="text-5xl text-white">Welcome!</h1>
-          <h1 className="text-3xl text-white">Bem-vindo!</h1>
+      <div className="flex">
+        <div className="w-2/6 h-60 gap-24 grid justify-center items-center">
+          <img
+            className="w-32 h-20 drop-shadow-weg-shadow mt-20"
+            src={WegLogo}
+            alt=""
+          />
+          <div className="grid font-bold">
+            <h1 className="text-5xl text-white">Welcome!</h1>
+            <h1 className="text-3xl text-white">Bem-vindo!</h1>
+          </div>
+          <div className="grid">
+            <h1 className="text-3xl font-bold text-white">SID</h1>
+            <h1 className="text-2xl font-bold text-slate-300">
+              Sustainable System
+            </h1>
+            <h1 className="text-2xl font-bold text-slate-300">of IT Demands</h1>
+            <h1 className="text-2xl font-bold text-slate-300">Management</h1>
+          </div>
         </div>
-        <div className="grid">
-          <h1 className="text-3xl font-bold text-white">SID</h1>
-          <h1 className="text-2xl font-bold text-slate-300">
-            Sustainable System
-          </h1>
-          <h1 className="text-2xl font-bold text-slate-300">of IT Demands</h1>
-          <h1 className="text-2xl font-bold text-slate-300">Management</h1>
-        </div>
-      </div>
-      <Container maxWidth="sm" sx={{ marginRight: "50px", marginTop: "-8rem" }}>
-        <Box
+        <Container
+          maxWidth="sm"
           sx={{
-            bgcolor: "#cfe8fc",
-            height: "520px",
-            width: "500px",
-            borderRadius: "10px",
+            marginTop: "10%",
+            display: "flex",
+            justifyContent: "end",
+            alignItems: "center",
           }}
         >
-          <div className="grid justify-center items-center">
-            <div className="grid gap-4">
-              <h1 className="font-bold flex justify-center items-center text-5xl m-12 text-blue-weg">
-                Login
-              </h1>
+          <Box
+            sx={{
+              bgcolor: "#cfe8fc",
+              height: "520px",
+              width: "500px",
+              borderRadius: "10px",
+            }}
+          >
+            <div className="grid justify-center items-center">
               <div className="grid gap-4">
-                <div className="flex justify-center items-center">
-                  <PersonOutlineOutlinedIcon
-                    sx={{ fontSize: 35, color: "#00579D" }}
-                  />
-                  <input
-                    className="w-96 h-14 outline-none text-base pl-4 rounded-lg border-2 border-slate-300"
-                    type="text"
-                    placeholder="Username"
-                  />
+                <h1 className="font-bold flex justify-center items-center text-5xl m-12 text-blue-weg">
+                  Login
+                </h1>
+                <div className="grid gap-4">
+                  <div className="flex justify-center items-center">
+                    <PersonOutlineOutlinedIcon
+                      sx={{ fontSize: 35, color: "#00579D" }}
+                    />
+                    <input
+                      className="w-96 h-14 outline-none text-base pl-4 rounded-lg border-2 border-slate-300"
+                      type="text"
+                      placeholder="Username"
+                    />
+                  </div>
+                  <div className="flex justify-center items-center">
+                    <LockOutlinedIcon sx={{ fontSize: 35, color: "#00579D" }} />
+                    <input
+                      className="w-96 h-14 outline-none text-base pl-4 rounded-lg border-2 border-slate-300"
+                      type="password"
+                      placeholder="Password"
+                    />
+                  </div>
                 </div>
-                <div className="flex justify-center items-center">
-                  <LockOutlinedIcon sx={{ fontSize: 35, color: "#00579D" }} />
-                  <input
-                    className="w-96 h-14 outline-none text-base pl-4 rounded-lg border-2 border-slate-300"
-                    type="password"
-                    placeholder="Password"
-                  />
+                <div className="flex justify-between items-center">
+                  <div className="flex gap-1 justify-center items-center">
+                    <Checkbox className="w-4 h-4" {...label} defaultChecked />
+                    <h1 className="text-gray-600 text-sm font-semibold">
+                      Lembrar de mim
+                    </h1>
+                  </div>
+                  <div>
+                    <h1 className="text-blue-weg cursor-pointer hover:text-sky-600 transition text-sm">
+                      Esqueceu a senha?
+                    </h1>
+                  </div>
                 </div>
-              </div>
-              <div className="flex justify-between items-center">
-                <div className="flex gap-1 justify-center items-center">
-                  <Checkbox className="w-4 h-4" {...label} defaultChecked />
-                  <h1 className="text-gray-600 text-sm font-semibold">
-                    Lembrar de mim
-                  </h1>
+                <div className="grid gap-4 justify-center items-center">
+                  <Link to="/minhas-demandas">
+                    <Button
+                      variant="contained"
+                      sx={{
+                        marginTop: "3rem",
+                        width: "140px",
+                        height: "45px",
+                        fontSize: "17px",
+                        fontWeight: "bold",
+                        textTransform: "none",
+                        backgroundColor: "#00579D",
+                      }}
+                    >
+                      Entrar
+                    </Button>
+                  </Link>
                 </div>
-                <div>
-                  <h1 className="text-blue-weg cursor-pointer hover:text-sky-600 transition text-sm">
-                    Esqueceu a senha?
-                  </h1>
-                </div>
-              </div>
-              <div className="grid gap-4 justify-center items-center">
-                <Link to="/minhas-demandas">
-                  <Button
-                    variant="contained"
-                    sx={{
-                      marginTop: "3rem",
-                      width: "140px",
-                      height: "45px",
-                      fontSize: "17px",
-                      fontWeight: "bold",
-                      textTransform: "none",
-                      backgroundColor: "#00579D",
-                    }}
-                  >
-                    Entrar
-                  </Button>
-                </Link>
               </div>
             </div>
-          </div>
-        </Box>
-      </Container>
+          </Box>
+        </Container>
+      </div>
     </div>
   );
 }
