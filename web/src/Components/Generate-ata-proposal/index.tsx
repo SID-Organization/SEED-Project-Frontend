367ef04b96d4630c7057b4381b75c074251edc55
HenriqueCole <henriquecolefernandes@gmail.com>
2022-12-12 14:11:53 -0300

    feat: generate new ata page

---
+++ web/src/Components/Generate-ata-proposal/index.tsx
@@ -0,0 +1,142 @@
+import {
+  Badge,
+  Box,
+  Divider,
+  FormControl,
+  InputLabel,
+  MenuItem,
+} from "@mui/material";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { useState } from "react";
+import MuiButton from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+
+import ProposalCard from "../Proposal-card";
+
+export default function GenerateAtaProposal() {
+  const [parecerComissao, setParecerComissao] = useState("");
+  const [considerations, setConsiderations] = useState("");
+
+  const handleChangeParecerComissao = (event: SelectChangeEvent) => {
+    setParecerComissao(event.target.value as string);
+  };
+
+  const actionsParecerComissao = [
+    {
+      action: "Aprovado",
+    },
+    {
+      action: "Reprovado",
+    },
+    {
+      action: "Mais informações",
+    },
+    {
+      action: "Business Case",
+    },
+  ];
+
+  const [publicada, setPublicada] = useState(false);
+  const [naoPublicada, setNaoPublicada] = useState(false);
+
+  const Button = styled(MuiButton)({
+    height: 50,
+    width: 150,
+    border: "1px solid #000",
+  });
+
+  return (
+    <div>
+      <div className="grid justify-center">
+        <div className="w-[65rem]">
+          <ProposalCard
+            id={1}
+            newPauta={true}
+            title="Proposta 1"
+            executionTime={10}
+            value={1000}
+            referenceDemand="Automatizar o processo de geração de atas"
+          />
+        </div>
+        <div
+          className="
+          md:grid md:grid-cols-2 md:gap-4 md:mt-5
+          mt-5
+          
+        "
+        >
+          <div className="grid">
+            <p className="font-roboto font-bold">Parecer da comissão</p>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl
+                fullWidth
+                sx={{
+                  width: 200,
+                }}
+              >
+                <Select
+                  sx={{
+                    height: 40,
+                  }}
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={parecerComissao}
+                  onChange={handleChangeParecerComissao}
+                >
+                  {actionsParecerComissao.map((action) => (
+                    <MenuItem value={action.action}>{action.action}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+          </div>
+          <div className="grid">
+            <p className="font-roboto font-bold">Considerações</p>
+            <textarea
+              className="w-[30rem] h-[4rem] border-2 border-[#000] rounded-md p-2 outline-blue-weg
+              min-h-[4rem]
+              "
+              placeholder="Digite aqui"
+              value={considerations}
+              onChange={(e) => setConsiderations(e.target.value)}
+            />
+          </div>
+          <div className="grid">
+            <p className="font-roboto font-bold mb-2">
+              Assunto registrado em ata
+            </p>
+            <div className="flex gap-2 mb-10">
+              <Button
+                onClick={() => {
+                  setPublicada(!publicada);
+                  setNaoPublicada(false);
+                }}
+                color={publicada ? "primary" : "secondary"}
+                variant={publicada ? "contained" : "outlined"}
+              >
+                Publicada
+              </Button>
+              <Button
+                onClick={() => {
+                  setNaoPublicada(!naoPublicada);
+                  setPublicada(false);
+                }}
+                color={naoPublicada ? "primary" : "secondary"}
+                variant={naoPublicada ? "contained" : "outlined"}
+              >
+                Não publicada
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Divider
+        sx={{
+          marginLeft: 30,
+          marginRight: 30,
+          marginBottom: 5,
+        }}
+      />
+    </div>
+  );
+}
--- web/src/pages/analista/generate-ata/index.tsx
+++ web/src/pages/analista/generate-ata/index.tsx
@@ -1,115 +1,49 @@
-import { Badge, Box, FormControl, InputLabel, MenuItem } from "@mui/material";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { Button } from "@mui/material";
 import { useParams } from "react-router";
-import { useState } from "react";
-import MuiButton from "@mui/material/Button";
-import { styled } from "@mui/material/styles";
 
-import ProposalCard from "../../../Components/Proposal-card";
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
+
+import GenerateAtaProposal from "../../../Components/Generate-ata-proposal";
 
 export default function GenerateAta() {
   const { id } = useParams();
   console.log("ID: ", id);
-
-  const [parecerComissao, setParecerComissao] = useState("");
-
-  const handleChangeParecerComissao = (event: SelectChangeEvent) => {
-    setParecerComissao(event.target.value as string);
-  };
-
-  const actionsParecerComissao = [
-    {
-      action: "Aprovado",
-      badgeColor: "#000",
-    },
-    {
-      action: "Reprovado",
-      badgeColor: "#C9c9c9",
-    },
-    {
-      action: "Mais informações",
-      badgeColor: "#000",
-    },
-    {
-      action: "Business Case",
-      badgeColor: "#000",
-    },
-  ];
-
-  const Button = styled(MuiButton)({
-    height: 60,
-    width: 150,
-    backgroundColor: "#FFF",
-    border: "1px solid #000",
-    color: "#000",
-    "&:hover": {
-      backgroundColor: "#FFF",
-    },
-  });
 
   return (
     <div className="grid items-center">
       <div className="flex justify-center items-center mb-5">
         <h1 className="font-bold text-3xl text-blue-weg mt-10">Ata 0003</h1>
       </div>
-      <div className="grid justify-center">
-        <div className="w-[65rem]">
-          <ProposalCard
-            id={1}
-            newPauta={true}
-            title="Proposta 1"
-            executionTime={10}
-            value={1000}
-            referenceDemand="Automatizar o processo de geração de atas"
-          />
-        </div>
-        <div
-          className="
-          md:grid md:grid-cols-2 md:gap-4 md:mt-5
-          mt-5
-          
-        "
+      <div className="grid">
+        <GenerateAtaProposal />
+        <GenerateAtaProposal />
+        <GenerateAtaProposal />
+      </div>
+      <div className="flex justify-end items-center mb-5 mr-10">
+        {/* Button to confirm action and end the circuit of the system */}
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "#0075B1",
+            width: "12rem",
+
+            "&:hover": {
+              backgroundColor: "#0075B1",
+            },
+          }}
+          className="w-[10rem] h-[2.5rem] mt-5"
+          startIcon={
+            <AddRoundedIcon
+              sx={{
+                color: "#fff",
+                height: "1.5rem",
+                width: "1.5rem",
+              }}
+            />
+          }
         >
-          <div className="grid">
-            <p className="font-roboto font-bold">Parecer da comissão</p>
-            <Box sx={{ minWidth: 120 }}>
-              <FormControl
-                fullWidth
-                sx={{
-                  width: 200,
-                }}
-              >
-                <Select
-                  sx={{
-                    height: 40,
-                  }}
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={parecerComissao}
-                  onChange={handleChangeParecerComissao}
-                >
-                  {actionsParecerComissao.map((action) => (
-                    <MenuItem value={action.action}>{action.action}</MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Box>
-          </div>
-          <div className="grid">
-            <p className="font-roboto font-bold">Considerações</p>
-            <textarea
-              className="w-[30rem] h-[4rem] border-2 border-[#000] rounded-md p-1 outline-blue-weg"
-              placeholder="Digite aqui"
-            ></textarea>
-          </div>
-          <div className="grid">
-            <p className="font-roboto font-bold">Assunto registrado em ata</p>
-            <div className="flex">
-              <Button variant="contained">Publicada</Button>
-              <Button variant="contained">Não publicada</Button>
-            </div>
-          </div>
-        </div>
+          Gerar nova ata
+        </Button>
       </div>
     </div>
   );
