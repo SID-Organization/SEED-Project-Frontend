eaf4e5db096a3d8a9387060fd441677739df8f33
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2023-03-30 21:49:08 -0300

    feat: adding security login, but the old login is commented

---
+++ web/src/pages/login/authService.jsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const url = "http://localhost:8443/login";
+
+class AuthService {
+  login(user) {
+    const config = {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+      }
+    };
+    return axios.post(url + "/auth", user, config);
+  }
+}
+
+export default new AuthService();
