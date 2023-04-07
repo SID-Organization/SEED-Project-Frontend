b0c100aa101080b0806a9b4f52db605270b6530f
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2023-04-06 21:19:41 -0300

    feat: requester can only edit a demand when its status is ABERTA

--- web/src/utils/User-Utils/index.js
+++ web/src/utils/User-Utils/index.js
@@ -1,14 +1,20 @@
 const getLoggedUser = () => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    return user;
-}
+  const user = JSON.parse(localStorage.getItem("user"));
+  return user;
+};
 
 const getLoggedUserId = () => {
-    const user = getLoggedUser();
-    return user.numeroCadastroUsuario;
-}
+  const user = getLoggedUser();
+  return user.numeroCadastroUsuario;
+};
+
+const getUserRole = () => {
+  const user = getLoggedUser();
+  return user.cargoUsuario;
+};
 
 export default {
-    getLoggedUser,
-    getLoggedUserId
-}
+  getLoggedUser,
+  getLoggedUserId,
+  getUserRole,
+};
