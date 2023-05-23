0ae223e7a16552a0f4d2501be74f615ee7ca3e3f
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2023-05-23 16:52:54 -0300

    feat: fixing speech

--- web/src/pages/requester/create-demand/first-step/index.jsx
+++ web/src/pages/requester/create-demand/first-step/index.jsx
@@ -1,19 +1,11 @@
 import "regenerator-runtime/runtime";
-
-// React
 import React, { useEffect, useRef, useState } from "react";
-
-// MUI
 import { InputAdornment } from "@mui/material";
 import ReactQuill from "react-quill";
 import MUISC from "../../../../styles/Mui-Styled-Components";
 import "react-quill/dist/quill.snow.css";
 import ReactQuillUtils from "../../../../utils/ReactQuill-Utils";
-import { SpeechRecognitionProvider } from "../../../../service/Voice-speech-Service/SpeechRecognitionContext.jsx";
 import VoiceSpeech from "../../../../Components/VoiceSpeech/index.jsx";
-
-const { quillModules, quillStyle } = ReactQuillUtils;
-
 import FontSizeUtils from "../../../../utils/FontSize-Utils";
 
 export default function FirstStep({ props }) {
@@ -22,6 +14,10 @@ export default function FirstStep({ props }) {
   const [textoSpeech, setTextoSpeech] = React.useState("");
   const [textoAgora, setTextoAgora] = React.useState("");
   const [textoInput, setTextoInput] = React.useState("");
+
+  const [textoSpeechObjetivo, setTextoSpeechObjetivo] = React.useState("");
+  const [textoAgoraObjetivo, setTextoAgoraObjetivo] = React.useState("");
+  const [textoInputObjetivo, setTextoInputObjetivo] = React.useState("");
 
   useEffect(() => {
     console.log("textoSpeech", textoSpeech);
@@ -44,7 +40,25 @@ export default function FirstStep({ props }) {
     props.setTitle(textoInput);
   }, [textoInput]);
 
-  setFonts(FontSizeUtils.getFontSizes());
+  useEffect(() => {
+    console.log("textoSpeech", textoSpeechObjetivo);
+    if (textoSpeechObjetivo !== "") {
+      if (props.proposalHTML === "") {
+        props.setProposalHTML(textoSpeechObjetivo);
+      } else {
+        props.setProposalHTML(props.proposalHTML + " " + textoSpeechObjetivo);
+      }
+    }
+    setTextoSpeechObjetivo("");
+  }, [textoSpeechObjetivo]);
+
+  useEffect(() => {
+    console.log(textoAgora);
+  }, [textoAgoraObjetivo]);
+
+  useEffect(() => {
+    setFonts(FontSizeUtils.getFontSizes());
+  }, [FontSizeUtils.getFontControl()]);
 
   const updateProposal = (e) => {
     props.setProposalHTML(e);
@@ -58,99 +72,100 @@ export default function FirstStep({ props }) {
     props.setFrequencyOfUseHTML(e);
   };
 
-  const handleSpeechInput = (text) => {
-    setTextoSpeech(text);
-  };
-
-  return (<div className="grid items-center justify-start gap-20">
-    <div className="grid gap-1">
-      <div className="mb-5 flex items-center justify-center">
-        <div className="mr-12 h-[5px] w-40 rounded-full bg-blue-weg" />
-        <h1
-          style={{ fontSize: fonts.xl }}
-          className="flex items-center justify-center font-roboto font-bold text-[#343434]"
-        >
-          Título
-        </h1>
-        <div className="ml-12 h-[5px] w-40 rounded-full bg-blue-weg" />
-        <VoiceSpeech setTexto={setTextoSpeech} setTitle={props.setTitle} setTextoFalado={setTextoAgora} />
+  return (
+    <div className="grid items-center justify-start gap-20">
+      <div className="grid gap-1">
+        <div className="mb-5 flex items-center justify-center">
+          <div className="mr-12 h-[5px] w-40 rounded-full bg-blue-weg" />
+          <h1
+            style={{ fontSize: fonts.xl }}
+            className="flex items-center justify-center font-roboto font-bold text-[#343434]"
+          >
+            Título
+          </h1>
+          <div className="ml-12 h-[5px] w-40 rounded-full bg-blue-weg" />
+          <VoiceSpeech setTexto={setTextoSpeech} setTextoFalado={setTextoAgora} />
+        </div>
+        <MUISC.TextField
+          id="outlined-textarea"
+          variant="outlined"
+          type="text"
+          multiline
+          maxRows={3}
+          value={props.title}
+          onChange={(e) => setTextoInput(e.target.value)}
+          onBlur={props.handleCreateDemand}
+          InputProps={{
+            startAdornment: <InputAdornment position="start" />
+          }}
+          helperText={props.title.length == 0 ? "O título é obrigatório" : props.title.length > 100}
+          error={props.title.length == 0 || props.title.length > 100}
+        />
       </div>
-      <MUISC.TextField
-        id="outlined-textarea"
-        variant="outlined"
-        type="text"
-        multiline
-        maxRows={3}
-        value={props.title}
-        onChange={(e) => setTextoInput(e.target.value)}
-        onBlur={props.handleCreateDemand}
-        InputProps={{
-          startAdornment: <InputAdornment position="start" />
-        }}
-        helperText={props.title.length == 0 ? "O título é obrigatório" : props.title.length > 100}
-        error={props.title.length == 0 || props.title.length > 100}
-      />
-    </div>
-    <div className="grid gap-1">
-      <div className="mb-5 flex items-center justify-center gap-5">
-        <div className="h-[5px] w-40 rounded-full bg-blue-weg" />
-        <h1
-          style={{ fontSize: fonts.xl }}
-          className="flex items-center justify-center font-roboto font-bold text-[#343434]"
-        >
-          Objetivo
-        </h1>
-        <div className="h-[5px] w-40 rounded-full bg-blue-weg" />
+      <div className="grid gap-1">
+        <div className="mb-5 flex items-center justify-center gap-5">
+          <div className="h-[5px] w-40 rounded-full bg-blue-weg" />
+          <h1
+            style={{ fontSize: fonts.xl }}
+            className="flex items-center justify-center font-roboto font-bold text-[#343434]"
+          >
+            Objetivo
+          </h1>
+          <div className="h-[5px] w-40 rounded-full bg-blue-weg" />
+          <VoiceSpeech
+            setTexto={setTextoSpeechObjetivo}
+            setTextoFalado={setTextoAgoraObjetivo}
+          />
+        </div>
+        <ReactQuill
+          value={props.proposalHTML}
+          onChange={updateProposal}
+          placeholder="Escreva a visão do negócio que vai resolver"
+          modules={ReactQuillUtils.quillModules}
+          style={ReactQuillUtils.quillStyle}
+          onBlur={props.handleCreateDemand}
+        />
       </div>
-      <ReactQuill
-        value={props.proposalHTML}
-        onChange={updateProposal}
-        placeholder="Escreva a visão do negócio que vai resolver"
-        modules={quillModules}
-        style={quillStyle}
-        onBlur={props.handleCreateDemand}
-      />
-    </div>
-    <div className="grid gap-1">
-      <div className="mb-5 flex items-center justify-center gap-5">
-        <div className="mr-3 h-[5px] w-40 rounded-full bg-blue-weg" />
-        <h1
-          style={{ fontSize: fonts.xl }}
-          className="flex items-center justify-center font-roboto font-bold text-[#343434]"
-        >
-          Situação atual
-        </h1>
-        <div className="ml-3 h-[5px] w-40 rounded-full bg-blue-weg" />
+      <div className="grid gap-1">
+        <div className="mb-5 flex items-center justify-center gap-5">
+          <div className="mr-3 h-[5px] w-40 rounded-full bg-blue-weg" />
+          <h1
+            style={{ fontSize: fonts.xl }}
+            className="flex items-center justify-center font-roboto font-bold text-[#343434]"
+          >
+            Situação atual
+          </h1>
+          <div className="ml-3 h-[5px] w-40 rounded-full bg-blue-weg" />
+        </div>
+        <ReactQuill
+          value={props.currentProblemHTML}
+          onChange={updateCurrentProblem}
+          placeholder="Descreva a situação atual da demanda."
+          onBlur={props.handleCreateDemand}
+          modules={ReactQuillUtils.quillModules}
+          style={ReactQuillUtils.quillStyle}
+        />
       </div>
-      <ReactQuill
-        value={props.currentProblemHTML}
-        onChange={updateCurrentProblem}
-        placeholder="Descreva a situação atual da demanda."
-        onBlur={props.handleCreateDemand}
-        modules={quillModules}
-        style={quillStyle}
-      />
-    </div>
-
-    <div className="mb-20 grid gap-1">
-      <div className="mb-5 flex items-center justify-center gap-5">
-        <div className="h-[5px] w-40 rounded-full bg-blue-weg" />
-        <h1
-          style={{ fontSize: fonts.xl }}
-          className="flex items-center justify-center font-roboto font-bold text-[#343434]"
-        >
-          Frequência de uso
-        </h1>
-        <div className="h-[5px] w-40 rounded-full bg-blue-weg" />
+      <div className="mb-20 grid gap-1">
+        <div className="mb-5 flex items-center justify-center gap-5">
+          <div className="h-[5px] w-40 rounded-full bg-blue-weg" />
+          <h1
+            style={{ fontSize: fonts.xl }}
+            className="flex items-center justify-center font-roboto font-bold text-[#343434]"
+          >
+            Frequência de uso
+          </h1>
+          <div className="h-[5px] w-40 rounded-full bg-blue-weg" />
+        </div>
+        <ReactQuill
+          onBlur={props.handleCreateDemand}
+          value={props.frequencyOfUseHTML}
+          onChange={updateFrequencyOfUse}
+          placeholder="Descreva a frequência de uso da demanda."
+          modules={ReactQuillUtils.quillModules}
+          style={ReactQuillUtils.quillStyle}
+        />
       </div>
-      <ReactQuill
-        onBlur={props.handleCreateDemand}
-        value={props.frequencyOfUseHTML}
-        onChange={updateFrequencyOfUse}
-        placeholder="Descreva a frequência de uso da demanda."
-        modules={quillModules}
-        style={quillStyle}
-      />
     </div>
-  </div>);
-}
+  );
+};
