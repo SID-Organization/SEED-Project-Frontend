72f6e91b210bda3a4727510d816304025f2ff2ed
LEONARDO GIUSEPPE DE SOUZA RAFAELLI <leonardo_rafaelli@estudante.sesisenai.org.br>
2023-05-23 21:34:57 -0300

    feat: add date utils to workflow table

--- web/src/Components/Workflow-table/index.jsx
+++ web/src/Components/Workflow-table/index.jsx
@@ -11,6 +11,9 @@ import { useState, useEffect } from "react";
 // Services
 import DemandLogService from "../../service/DemandLog-Service";
 
+// Utils
+import DateUtils from "../../utils/Date-Utils";
+
 // Renderizador de células normais
 const renderCellTooltip = (params) => (
   <Tooltip title={params.value} enterDelay={820}>
@@ -20,7 +23,7 @@ const renderCellTooltip = (params) => (
 
 // Renderizador de células de data
 const renderDateCell = (params) => {
-  const date = params.value ? getTableDate(params.value) : "Indefinido";
+  const date = params.value ? DateUtils.formatDateTime(params.value) : "Indefinido";
   return (
     <Tooltip title={date} enterDelay={820}>
       <p className="text-[11px]">{date}</p>
@@ -28,22 +31,6 @@ const renderDateCell = (params) => {
   );
 };
 
-// Função para formatar a data
-function getTableDate(date) {
-  const dateObject = new Date(date);
-  const dateString =
-    dateObject.toLocaleDateString("pt-BR", {
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-    }) +
-    " - " +
-    dateObject.toLocaleTimeString("pt-BR", {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-  return dateString;
-}
 
 function CustomToolbar() {
   return (
@@ -153,7 +140,7 @@ export default function WorkflowTable({ demandId }) {
           prazo: wfdata.prazoHistorico,
           tarefa: wfdata.tarefaHistoricoWorkflow,
           responsavel: wfdata.nomeResponsavel,
-          acao: wfdata.acaoFeitaHistorico,
+          acao: wfdata.acaoFeitaHistorico ?? "- - - - -",
           status: wfdata.statusWorkflow,
           versao: wfdata.versaoHistorico,
         };
--- web/src/utils/Date-Utils/index.js
+++ web/src/utils/Date-Utils/index.js
@@ -1,21 +1,40 @@
-const formatDate = (date) => {
-    const newDate = date
-        .split("T")[0]
-        .split("-")
-        .reverse()
-        .join("/");
-    return newDate;
-};
-
-const formatDateFromDB = (date) => {
+const formatDateForDB = (date) => {
     if (!date) return "";
     const newDate = date
         .split("T")[0];
-    
+
     return newDate;
 };
 
+function formatDate(date) {
+    const dateObject = new Date(date);
+    const dateString =
+        dateObject.toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+        })
+    return dateString;
+}
+
+function formatDateTime(date) {
+    const dateObject = new Date(date);
+    const dateString =
+        dateObject.toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+        })
+        + " - " +
+        dateObject.toLocaleTimeString("pt-BR", {
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+    return dateString;
+}
+
 export default {
     formatDate,
-    formatDateFromDB
+    formatDateTime,
+    formatDateForDB
 }
