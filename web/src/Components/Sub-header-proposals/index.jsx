fe34cb5de2b7c0b37e94508b4a59a5961df16f9f
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2023-06-05 21:50:48 -0300

    feat: keeping doing translations

--- web/src/Components/Sub-header-proposals/index.jsx
+++ web/src/Components/Sub-header-proposals/index.jsx
@@ -7,8 +7,14 @@ import CreateNewProposalButton from "../CreateNewProposal-Button";
 
 // Services
 import DemandService from "../../service/Demand-Service";
+import TranslationJson from "../../API/Translate/components/subHeaderPautas.json";
+import TranslateUtils from "../../utils/Translate-Utils/index.js";
 
 export default function SubHeaderPautas(props) {
+
+  const translate = TranslationJson;
+  const language = TranslateUtils().getLanguage();
+
   const [demandsTitle, setDemandsTitle] = useState([]);
 
   useEffect(() => {
@@ -24,7 +30,7 @@ export default function SubHeaderPautas(props) {
     <div>
       <div className="flex h-[5rem] items-center justify-between shadow-page-title-shadow">
         <h1 className="ml-[8rem] font-roboto text-3xl font-bold text-dark-blue-weg">
-          Propostas
+          {translate["Propostas"][language] ?? "Propostas"}
         </h1>
         <div className="mr-10 flex gap-16">
           <CreateNewProposalButton demandsTitle={demandsTitle} />
