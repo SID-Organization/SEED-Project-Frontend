2000af5eed29e45ac1bb6af02f7d688dd5c721cd
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2023-06-15 16:43:49 -0300

    feat: opening the modal of pautas proposals

--- web/src/Components/Calendar/index.jsx
+++ web/src/Components/Calendar/index.jsx
@@ -6,35 +6,6 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import TranslationJson from "../../API/Translate/components/calendar.json";
 import { TranslateContext } from "../../contexts/translate/index.jsx";
 
-//MUI
-import {
-  Box,
-  Button,
-  Modal,
-  TextField,
-  InputAdornment,
-  Autocomplete,
-} from "@mui/material";
-import CheckRoundedIcon from "@mui/icons-material/CheckRounded";
-
-//Utils
-import FontSizeUtils from "../../utils/FontSize-Utils";
-import DatePicker from "../Date-picker";
-import VoiceSpeech from "../VoiceSpeech";
-
-const modalStyled = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 1250,
-  height: 650,
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  p: 4,
-  borderRadius: "10px",
-};
-
 export default function Calendar(props) {
   const translate = TranslationJson;
   const [language] = useContext(TranslateContext);
@@ -53,7 +24,6 @@ export default function Calendar(props) {
     id: 1,
     text: "",
   });
-  const [fonts, setFonts] = useState(FontSizeUtils.getFontSizes());
 
   const { pautas } = props;
 
@@ -70,10 +40,6 @@ export default function Calendar(props) {
       );
     }
   }, [foruns]);
-
-  useEffect(() => {
-    setFonts(FontSizeUtils.getFontSizes());
-  }, [FontSizeUtils.getFontControl()]);
 
   useEffect(() => {
     const convertedMeetings = pautas.map((pauta) => ({
@@ -107,6 +73,8 @@ export default function Calendar(props) {
     setSelectedEvent(pautaId);
     setIsModalOpen(true); // Open the modal when an event is clicked
     console.log("PAUTA ID: ", pautaId);
+    props.setPautaId(pautaId);
+    props.setIsModalOpen(false);
   };
 
   const convertDateFormat = (date) => {
@@ -119,143 +87,12 @@ export default function Calendar(props) {
       <FullCalendar
         plugins={[dayGridPlugin]}
         initialView="dayGridMonth"
-        weekends={false}
+        weekends={true}
         events={meetings}
         dayMaxEventRows={3}
         eventClick={handleEventClick}
         locale={language}
       />
-      {isModalOpen && (
-        <Modal
-          style={{ zIndex: 1 }}
-          open={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={modalStyled}>
-            <div className="grid w-full gap-10 font-roboto">
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-5">
-                  <h1 style={{ fontSize: fonts.base }} className="font-bold">
-                    {translate["Data da reunião"]?.[language]}:{" "}
-                  </h1>
-                  <DatePicker
-                    searchValue={meetingDate}
-                    setSearchValue={setMeetingDate}
-                  />
-                </div>
-              </div>
-              <div className="flex items-center justify-between gap-12">
-                <div className="flex items-center gap-20">
-                  <div className="flex items-center gap-5">
-                    <h1 style={{ fontSize: fonts.base }} className="font-bold">
-                      {translate["Horário:"]?.[language] ?? "Horário:"}
-                    </h1>
-                    <TextField
-                      id="outlined-basic"
-                      label={translate["Início"]?.[language] ?? "Início"}
-                      variant="outlined"
-                      type="time"
-                      value={meetingStartTime}
-                      onChange={(e) => setMeetingStartTime(e.target.value)}
-                      InputProps={{
-                        startAdornment: <InputAdornment position="start" />,
-                      }}
-                      sx={{
-                        width: "10rem",
-                        height: "3rem",
-                      }}
-                    />
-                    <TextField
-                      id="outlined-basic"
-                      label={translate["Término"]?.[language] ?? "Término"}
-                      variant="outlined"
-                      type="time"
-                      value={meetingEndTime}
-                      onChange={(e) => setMeetingEndTime(e.target.value)}
-                      InputProps={{
-                        startAdornment: <InputAdornment position="start" />,
-                      }}
-                      sx={{
-                        width: "10rem",
-                        height: "3rem",
-                      }}
-                    />
-                  </div>
-                  <Autocomplete
-                    disablePortal
-                    id="combo-box-demo"
-                    options={comissoes}
-                    value={selectedForum}
-                    onChange={(event, newValue) => {
-                      setSelectedForum(newValue);
-                    }}
-                    sx={{ width: 300 }}
-                    renderInput={(params) => (
-                      <TextField
-                        {...params}
-                        label={translate["Comissão"]?.[language] ?? "Comissão"}
-                      />
-                    )}
-                  />
-                </div>
-                <TextField
-                  id="outlined-basic"
-                  label={
-                    translate["Procurar por título"]?.[language] ??
-                    "Procurar por título"
-                  }
-                  variant="outlined"
-                  value={searchTitle}
-                  InputProps={{
-                    endAdornment: (
-                      <VoiceSpeech
-                        setTexto={setSearchByTitleSpeech}
-                        speechId={1}
-                      />
-                    ),
-                  }}
-                  onChange={(e) => setSearchTitle(e.target.value)}
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center justify-center gap-5">
-                  <div className="flex items-center justify-center gap-5">
-                    <div className="h-[1.5px] w-10 rounded-full bg-light-blue-weg" />
-                    <h1 style={{ fontSize: fonts.xl }}>
-                      {translate["Selecione as propostas"]?.[language] ??
-                        "Selecione as propostas"}
-                    </h1>
-                    <div className="h-[1.5px] w-10 rounded-full bg-light-blue-weg" />
-                  </div>
-                </div>
-                <div
-                  className="grid max-h-[21rem] gap-5 overflow-y-scroll scrollbar-thin
-                  scrollbar-thumb-[#a5a5a5] scrollbar-thumb-rounded-full scrollbar-w-2"
-                >
-                  {/* {readyProposals.length > 0 &&
-                    readyProposals
-                      .filter((item) => {
-                        if (!searchTitle || searchTitle.length < 3) return true;
-                        return item.demandaPropostaTitulo
-                          .toLowerCase()
-                          .includes(searchTitle.toLowerCase());
-                      })
-                      .map((item, i) => (
-                        <NewPautaProposalCard
-                          key={i}
-                          selectedProposals={selectedProposals}
-                          setSelectedProposals={setSelectedProposals}
-                          proposal={item}
-                        />
-                      ))} */}
-                </div>
-              </div>
-            </div>
-          </Box>
-        </Modal>
-      )}
     </div>
   );
 }
--- web/src/Components/Date-picker/index.jsx
+++ web/src/Components/Date-picker/index.jsx
@@ -1,27 +1,45 @@
-import react from 'react'
+import react from "react";
 
-import { TextField, InputAdornment } from '@mui/material'
-import { styled } from '@mui/material/styles'
+import { TextField, InputAdornment } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 const DateField = styled(TextField)({
-    width: "14rem",
-    marginBottom: "1rem",
-    height: "3rem",
+  width: "14rem",
+  marginBottom: "1rem",
+  height: "3rem",
 });
 
 export default function DatePicker(props) {
-    return (
+  console.log("props.meetingdata: ", props.searchValue);
+  return (
+    <>
+      {props.viewOnly === true ? (
         <DateField
-            id="outlined-basic"
-            variant="outlined"
-            placeholder="dd/mm/aaaa"
-            type="date"
-            label={props.label}
-            value={props.searchValue}
-            onChange={(e) => props.setSearchValue(e.target.value)}
-            InputProps={{
+          id="outlined-basic"
+          variant="outlined"
+          placeholder="dd/mm/aaaa"
+          type="date"
+          label={props.label}
+          value={props.searchValue}
+          disabled
+          InputProps={{
             startAdornment: <InputAdornment position="start" />,
-            }}
+          }}
         />
-    )
+      ) : (
+        <DateField
+          id="outlined-basic"
+          variant="outlined"
+          placeholder="dd/mm/aaaa"
+          type="date"
+          label={props.label}
+          value={props.searchValue}
+          onChange={(e) => props.setSearchValue(e.target.value)}
+          InputProps={{
+            startAdornment: <InputAdornment position="start" />,
+          }}
+        />
+      )}
+    </>
+  );
 }
