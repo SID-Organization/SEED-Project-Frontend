099fdce811267cc3a301a6ebd27c80b5ed02f232
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2023-06-16 20:20:12 -0300

    fix: english translation bottom button

--- web/src/pages/requester/create-demand/index.jsx
+++ web/src/pages/requester/create-demand/index.jsx
@@ -26,14 +26,12 @@ import FontSizeUtils from "../../../utils/FontSize-Utils";
 import DemandUtils from "../../../utils/Demand-Utils";
 
 //Translation
-import TranslationJson from "../../../API/Translate/pages/requester/createDemand.json"
+import TranslationJson from "../../../API/Translate/pages/requester/createDemand.json";
 import { TranslateContext } from "../../../contexts/translate/index";
 
 const { removeHTML } = ReactQuillUtils;
 
-
 export default function CreateDemand({ isEditting }) {
-
   const translate = TranslationJson;
   const [language] = useContext(TranslateContext);
 
@@ -80,7 +78,6 @@ export default function CreateDemand({ isEditting }) {
   const [demandUpdateId, setDemandUpdateId] = useState("");
 
   const [createDemandSucceed, setCreateDemandSucceed] = useState(true);
-
 
   // Usuário logado
   const [user, setUser] = useState(UserUtils.getLoggedUser());
@@ -154,7 +151,6 @@ export default function CreateDemand({ isEditting }) {
     setConfirmDemand(false);
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
-
 
   const handleCreateDemand = async (finish = false) => {
     if (!finish && isEditting) return;
@@ -260,17 +256,17 @@ export default function CreateDemand({ isEditting }) {
   };
 
   const handleFinishDemand = (formData) => {
-    DemandService.updateDemand(demandUpdateId, formData)
-      .then(res => {
-        if ([200, 201].includes(res.status)) {
-          DemandService.updateDemandStatus(demandUpdateId, "ABERTA")
-            .then(res => {
-              if ([200, 201].includes(res.status)) {
-                setCreateDemandSucceed(true)
-              }
-            });
-        }
-      });
+    DemandService.updateDemand(demandUpdateId, formData).then((res) => {
+      if ([200, 201].includes(res.status)) {
+        DemandService.updateDemandStatus(demandUpdateId, "ABERTA").then(
+          (res) => {
+            if ([200, 201].includes(res.status)) {
+              setCreateDemandSucceed(true);
+            }
+          }
+        );
+      }
+    });
     if (createDemandSucceed) {
       setTimeout(() => {
         navigate("/demandas");
@@ -372,7 +368,11 @@ export default function CreateDemand({ isEditting }) {
     return { name, size: fileSize };
   }
 
-  const steps = [(translate["Dados gerais"]?.[language] ?? "Dados gerais"), (translate["Benefícios"]?.[language] ?? "Benefícios"), (translate["Arquivos"]?.[language] ?? "Arquivos")];
+  const steps = [
+    translate["Dados gerais"]?.[language] ?? "Dados gerais",
+    translate["Benefícios"]?.[language] ?? "Benefícios",
+    translate["Arquivos"]?.[language] ?? "Arquivos",
+  ];
 
   const firstStepProps = {
     title,
@@ -421,7 +421,8 @@ export default function CreateDemand({ isEditting }) {
       <div className="mb-7">
         <div className="flex h-[5rem] items-center justify-around shadow-page-title-shadow">
           <h1 className="font-roboto text-3xl font-bold text-dark-blue-weg">
-            {translate['Criar nova demanda']?.[language] ?? "Criar nova demanda"}
+            {translate["Criar nova demanda"]?.[language] ??
+              "Criar nova demanda"}
           </h1>
         </div>
       </div>
@@ -457,7 +458,9 @@ export default function CreateDemand({ isEditting }) {
             onClick={handleNext}
             disabled={title.length == 0}
           >
-            {activeStep === steps.length - 1 ? (translate["Finalizar"]?.[language]) : (translate["Próximo"]?.[language])}
+            {activeStep === steps.length - 1
+              ? translate["Finalizar"]?.[language] ?? "Finalizar"
+              : translate["Próximo"]?.[language] ?? "Próximo"}
           </Button>
         </div>
       </div>
