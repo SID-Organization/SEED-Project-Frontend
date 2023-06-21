62a3b2f1b239d18721ad05e3767663d97c318474
gustavo_zapella <gu.zapella@gmail.com>
2023-06-21 15:59:11 -0300

    feat: Added all the functions to open the excel table and added a button for open the excel table

--- web/src/Components/Header/index.jsx
+++ web/src/Components/Header/index.jsx
@@ -413,6 +413,8 @@ export default function PrimarySearchAppBar() {
     }
   }, [chatUsers]);
 
+  const listaDemandas = [1];
+
   //UseEffect para buscar todos os usuários do banco de dados
   useEffect(() => {
     ChatService.getChatByUserId(UserUtils.getLoggedUserId()).then((users) => {
